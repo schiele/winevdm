8e147f76ad7e92c64576c3acc789eff3c43e1a55
Martin Payne <development@martinpayne.me.uk>
2017-12-13 10:24:32 +0000

    user.exe16: Fix handling of 16-bit HBITMAPs in InsertMenu16.
    Signed-off-by: Martin Payne <development@martinpayne.me.uk>
    Signed-off-by: Alexandre Julliard <julliard@winehq.org>

--- user/user.c
+++ user/user.c
@@ -2704,6 +2704,10 @@ BOOL16 WINAPI InsertMenu16( HMENU16 hMenu, UINT16 pos, UINT16 flags,
         id32 = HMENU_32(id);
     if (IS_MENU_STRING_ITEM(flags) && data)
         return InsertMenuA( HMENU_32(hMenu), pos32, flags, id32, MapSL(data) );
+
+    /* If "data" is an HBITMAP, the high WORD will contain the application's DGROUP selector if the
+     * application cast (LPSTR)hBitmap rather than (LPSTR)(LONG)hBitmap. */
+    if (flags & MF_BITMAP) data = (SEGPTR)HBITMAP_32(LOWORD(data));
     return InsertMenuA( HMENU_32(hMenu), pos32, flags, id32, (LPSTR)data );
 }
 
